7a082cae78ead7c4f4a4dfd6a0ac4fe58cf2be70
liuhy14 <google-dl-platform@googlegroups.com>
2019-05-12 09:20:48 +0000

    fix typo

--- WSDAN_model/train_wsdan.py
+++ WSDAN_model/train_wsdan.py
@@ -114,7 +114,7 @@ def main():
     # Load dataset
     ##################################
 
-    train_dataset, validate_dataset = INAT(data_root_train, train_file, , image_size, is_train=True), \
+    train_dataset, validate_dataset = INAT(data_root_train, train_file, image_size, is_train=True), \
                                       INAT(data_root_train, val_file, image_size, is_train=False)
 
     train_loader, validate_loader = DataLoader(train_dataset, batch_size=options.batch_size, shuffle=True,
